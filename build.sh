3347ca4fb0c7ec294d04031e81bb2f65d4954581
dorajimooc <168262737+acornjelly2205@users.noreply.github.com>
2026-05-08 14:15:36 +0900

    Update Ginkgo_DIR variable for build script

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+set -e
+
+GINKGO_DIR="${GINKGO_DIR:-$HOME/ginkgo/install}"
+
 rm -rf build
 mkdir build
 cd build
-cmake .. -DGinkgo_DIR=/home/inseo764/TC_BELL/docker/ICTC/install/ginkgo/lib/cmake/Ginkgo
-make -j
+cmake .. -DGinkgo_DIR="${GINKGO_DIR}/lib/cmake/Ginkgo"
+make -j$(nproc)
